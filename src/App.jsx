071e537a3e7eba4b6cbc4135e36d7c15d8f3cd68
fraidertara10 <fraidertarazona10@gmail.com>
2025-08-29 15:39:28 -0500

    form register

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './pages/Auth/login' 
+import './pages/Auth/login.jsx' 
 import Login from './pages/Auth/login.jsx';
+import Register from './pages/Auth/register.jsx';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
   
-     <Login/>
-    
+    // <Login/>
+    <Register/>
   )
 }
 
