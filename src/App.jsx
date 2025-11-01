6076bc7606ce5e18fa5a9063ca07b145917f568c
mateoestiven <pallaresjacomem@gmail.com>
2025-10-31 19:18:54 -0500

    STG-43 historia de usuario terminada

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login/>}></Route>
-        
+        <Route path="/" element={<Home/>}></Route>
         <Route path="/login" element={< Login/>}></Route>
         <Route path="/register" element={< Register/>}></Route>
         <Route path="/forgotPassword" element={< ForgotPassword/>}></Route>
